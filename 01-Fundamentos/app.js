900f405c601fc6ef452544000207ee9f880e8e29
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-02-06 17:29:44 -0600

    Clase 02.09 terminada, base del repositorio

---
+++ 01-Fundamentos/app.js
@@ -0,0 +1,23 @@
+
+var primer_Nombre = 'Grover'
+console.log(primer_Nombre)
+
+var edad = 34
+var sueldo = 1800.99
+
+console.log(edad, sueldo)
+
+var tiene_Trabajo = true
+
+console.log(tiene_Trabajo)
+
+/**********************
+Operaciones matemáticas
+**********************/
+
+var edad1 = 34
+var edad2 = 28
+
+var diferenciaEdades = edad1 - edad2
+
+console.log(diferenciaEdades)
