7a569c5db8718be18cfc82903b8aa4925142d0af
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-02-07 23:30:44 -0600

    Clase 04-05 terminada

---
+++ 03-Sentencias-Bucles/04_switch.js
@@ -0,0 +1,20 @@
+
+var mes = 5
+
+switch (mes) {
+    case 1:
+        console.log('Enero')
+        break
+    case 2:
+        console.log('Febrero')
+        break
+    case 3:
+        console.log('Marzo')
+        break
+    case 4:
+        console.log('Abril')
+        break
+    default:
+        console.log('Otro mes')
+        break
+}
