083d28257a7268ee3f0f0e4a179d621f93e26f49
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-06-07 20:46:08 -0600

    Clase 85 terminada

---
+++ 07-ES6/app.js
@@ -0,0 +1,16 @@
+// ES5 - Variables
+var nombre5 = 'Pablo'
+console.log(nombre5)
+
+var edad5 = 35
+nombre5 = 'Carlos'
+console.log(nombre5)
+
+// ES6 - Variables
+const nombre6 = 'Pedro'
+console.log(nombre6)
+
+let edad6 = 28
+//nombre6 = 'Ana'
+edad6 = 30
+console.log(edad6)
