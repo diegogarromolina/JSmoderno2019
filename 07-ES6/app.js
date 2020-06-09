73d61531d8ce67daffe2efe0008290bb3104e4d1
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-06-08 22:03:51 -0600

    Clase 87 terminada

--- 07-ES6/app.js
+++ 07-ES6/app.js
@@ -1,4 +1,6 @@
-// ES5 - Variables
+/***********
+ * ES5 - Variables
+ */
 var nombre5 = 'Pablo'
 console.log(nombre5)
 
@@ -6,7 +8,9 @@ var edad5 = 35
 nombre5 = 'Carlos'
 console.log(nombre5)
 
-// ES6 - Variables
+/*****************
+ * ES6 - Variables
+ */
 const nombre6 = 'Pedro'
 console.log(nombre6)
 
@@ -14,3 +18,47 @@ let edad6 = 28
 //nombre6 = 'Ana'
 edad6 = 30
 console.log(edad6)
+
+/********************
+ * Bloques y alcance de las variables
+ */
+let test
+test = 10
+function prueba() {
+    let test
+    test = 15
+}
+prueba()
+console.log(test)
+
+{
+    const a = 3
+    let b = 4
+    var c = 5
+}
+
+/*****
+ * Con const y let las variables solo tiene alcance dentro del
+ * bloque donde son declaradas. Con var las variables tienen
+ * alcance en todo el script */
+//console.log(a + b)
+console.log(c)
+
+/*****************
+ * Template Strings
+ */
+
+let nombre = 'Diego'
+let apellido = 'Garro'
+let ciudad = 'Quepos'
+const nacimiento = 1986
+
+function calcularEdad(year) {
+    return 2020 - year
+}
+
+// ES5
+console.log(nombre + ' ' + apellido + ', nació en ' + ciudad + ', y su edad es ' + calcularEdad(nacimiento) )
+
+// ES6
+console.log(`${nombre} ${apellido}, nació en ${ciudad}, y su edad es ${calcularEdad(nacimiento)}`)
