8a631f5270409594d9d7b8d7ef2c531930198fd9
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-06-16 18:38:54 -0600

    Clase 106 terminada

--- 08-Objetos/app.js
+++ 08-Objetos/app.js
@@ -77,15 +77,87 @@
  * Métodos
  */
 
-let persona = {
-    nombre: 'Diego',
-    edad: '30',
-    sueldo: 1200,
-    metodoPrueba: function() {
-        // console.log('Escribiendo desde el método de prueba')
-        return 'Resultado desde prueba'
-    }
+// let persona = {
+//     nombre: 'Diego',
+//     edad: 30,
+//     sueldo: 1200,
+//     metodoPrueba: function() {
+//         // console.log('Escribiendo desde el método de prueba')
+//         return 'Resultado desde prueba'
+//     },
+//     cambiarEdad: function(e) {
+//         this.edad = this.edad + e
+//     }
+// }
+
+// let result = persona.metodoPrueba()
+// console.log(result)
+// persona.cambiarEdad(10)
+// console.log(persona.edad)
+
+/**********************
+ * Objeto String
+ */
+
+// let nombre = 'Diego Garro'
+
+// console.log(nombre.length)
+// console.log(nombre.toUpperCase())
+
+/**********************
+ * Destructuración de objetos
+ */
+
+const getPersonaDatos = () => {
+    const respuesta = [
+            {
+                codigo: 'Diego',
+                data: {
+                    persona: {
+                        nombre: 'Diego',
+                        direccion: {
+                            ciudad: 'San José',
+                            pais: 'Costa Rica',
+                        }
+                    }
+                }
+            },
+            {
+                codigo: 200,
+                data: {
+                    persona: {
+                        nombre: 'Melina',
+                        direccion: {
+                            ciudad: 'San José',
+                            pais: 'EEUU',
+                        }
+                    }
+                }
+            },
+            {
+                codigo: 400,
+                data: {
+                    persona: {
+                        nombre: 'Samantha',
+                        direccion: {
+                            ciudad: 'Hong Kong',
+                            pais: 'China',
+                        }
+                    }
+                }
+            },
+        ]
+    return respuesta
 }
 
-let result = persona.metodoPrueba()
-console.log(result)
+
+
+let {codigo : status, data : {persona : {nombre : num}}} = getPersonaDatos()[0]
+
+// console.log(respuesta)
+console.log(status)
+console.log(num)
+
+for (const {codigo : status, data : {persona : {nombre : nom}}} of getPersonaDatos()) {
+    console.log(nom)
+}
