2b58a22477064e1fcf3f7f70f3fe1e90ce2ae45a
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-06-21 23:57:22 -0600

    Clase 153 terminada

--- 14-JS-Asincrono/app.js
+++ 14-JS-Asincrono/app.js
@@ -26,24 +26,60 @@
  * Simulación
  */
 
-const getUsuarios = () => {
-    setTimeout(() => {
-        const userIDs = [101, 102, 103, 105]
-        console.log(userIDs)
+// const getUsuarios = () => {
+//     setTimeout(() => {
+//         const userIDs = [101, 102, 103, 105]
+//         console.log(userIDs)
 
-        setTimeout((id) => {
-            const usuario = {
-                email: 'diego@diego.com',
-                nombre: 'Diego',
-            }
-            console.log(`${id} - ${usuario.email} - ${usuario.nombre}`)
+//         setTimeout((id) => {
+//             const usuario = {
+//                 email: 'diego@diego.com',
+//                 nombre: 'Diego',
+//             }
+//             console.log(`${id} - ${usuario.email} - ${usuario.nombre}`)
 
-            setTimeout((idPermiso) => {
-                const permisos = ['admin', 'creador']
-                console.log(permisos)
-            }, 1500, userIDs[2])
-        }, 1500, userIDs[2])
-    }, 1500)
-}
+//             setTimeout((idPermiso) => {
+//                 const permisos = ['admin', 'creador']
+//                 console.log(permisos)
+//             }, 1500, userIDs[2])
+//         }, 1500, userIDs[2])
+//     }, 1500)
+// }
 
-getUsuarios()
+// getUsuarios()
+
+/********************
+ * Ejemplos de uso de promesas
+ */
+
+// Ejemplo 01
+
+let promesa = new Promise((resolve, reject) => {
+    if (true) {
+        resolve('La operación fue exitosa')
+    } else {
+        reject('Hubo un error')
+    }
+})
+
+promesa.then(response => {
+    console.log('Response: ' + response)
+}).catch(error => {
+    console.log('Error: ' + error)
+})
+
+// Ejemplo 02
+
+let miPromesa = Promise.resolve('comida')
+miPromesa.then(resp => console.log(resp))
+
+// Ejemplo 03
+
+var miPromesa2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(4))
+}, 2000)
+
+miPromesa2.then(resp => {
+    resp += 5
+    console.log(resp)
+})
